eb3b38a830a4fa435b4390f29de3191f6e97601a
edison.martins <martinsegmj@gmail.com>
2015-08-21 12:39:38 -0300

    Preparado para Welcomo page

--- BeginningWicket/src/main/java/org/apache/wicket/app/cfg/WicketApplication.java
+++ BeginningWicket/src/main/java/org/apache/wicket/app/cfg/WicketApplication.java
@@ -1,10 +1,18 @@
 package org.apache.wicket.app.cfg;
 
 import org.apache.wicket.Page;
+import org.apache.wicket.RuntimeConfigurationType;
 import org.apache.wicket.app.ui.Login;
 import org.apache.wicket.protocol.http.WebApplication;
 
 public class WicketApplication extends WebApplication {
+	
+	public WicketApplication() {
+		
+		//Development or Deplyment  configuration
+		System.out.println("-- WICKET APPLICATION: DEVELOPMENT MODE CONFIGURATION --");
+		setConfigurationType(RuntimeConfigurationType.DEVELOPMENT);
+	}
 
 	@Override
 	public Class<? extends Page> getHomePage() {
